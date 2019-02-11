1819b39bbe5afa0aab399a9725702e3189a0cfca
whine <sashazemzer@gmail.com>
2019-02-11 16:25:55 +0300

    пересоздал с помощью vue cli

--- backend/src/main/java/org/donntu/ai/backend/controller/HomeController.java
+++ backend/src/main/java/org/donntu/ai/backend/controller/HomeController.java
@@ -1,8 +1,12 @@
 package org.donntu.ai.backend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-
+    @GetMapping
+    public String test(){
+        return "Опа здарова тебе с сервачка, братан";
+    }
 }
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,18 @@
+const BASE_URL = '/';
+
 module.exports = {
-  // настройки...
+  publicPath: BASE_URL,
+  assetsDir: 'static',
+  productionSourceMap: false,
+  devServer: {
+    compress: true,
+    open: true,
+    host: 'localhost',
+    port: 8081,
+    proxy: {
+      '': {
+        target: 'http://localhost:8080',
+      },
+    },
+  },
 };
